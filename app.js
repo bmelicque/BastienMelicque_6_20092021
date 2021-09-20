06735c776b2287049c630e716bfedeee69fbf38e
Bastien Mélicque <bmelicque@gmail.com>
2021-09-20 13:15:22 +0200

    User routes

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER_PASS}@cluster0.ljsry.mongodb.net/piiquante?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
