442b5b2388a79d09d276963d730f74a44c1f4544
Bastien Mélicque <bmelicque@gmail.com>
2021-09-20 12:08:08 +0200

    Working on .env

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require("dotenv");
-dotenv.config();
-const DB_USER_PASS = process.env.DB_USER_PASS;
+require('dotenv').config();
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${DB_USER_PASS}@cluster0.ljsry.mongodb.net/piiquante?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://${process.env.DB_USER_PASS}@cluster0.ljsry.mongodb.net/piiquante?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
