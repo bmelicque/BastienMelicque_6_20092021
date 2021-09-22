0f4b7b523a4b9e8fc3eb7746f388f17e25dcb74e
Bastien Mélicque <bmelicque@gmail.com>
2021-09-22 15:46:52 +0200

    Rewrote user controller with await

--- controllers/user.js
+++ controllers/user.js
@@ -3,26 +3,28 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
+exports.signup = async (req, res, next) => {
+    try {
+        const hash = await bcrypt.hash(req.body.password, 10);
         const user = new User({
             email: req.body.email,
             password: hash
         });
-        user.save()
-        .then(() => res.status(201).json({message: 'Utilisateur créé avec succès !'}))
-        .catch(error => res.status(400).json({error}))
-    })
-    .catch(error => res.status(500).json({error}))
+        try {
+            await user.save();
+            res.status(201).json({message: 'Utilisateur créé avec succès !'});
+        } catch (error) {
+            res.status(400).json({error});
+        }
+    } catch (error) {
+        res.status(500).json({error});
+    }
 }
 
 exports.login = (req, res, next) => {
-    User.findOne({email: req.body.email})
-    .then(user => {
-        if (!user) {
-            return res.status(401).json({message: 'Utilisateur non trouvé !'});
-        }
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if (!user) return res.status(401).json({message: 'Utilisateur non trouvé !'});
         res.status(200).json({
             userId: user._id,
             token: jwt.sign(
@@ -31,6 +33,7 @@ exports.login = (req, res, next) => {
                 {expiresIn: '24h'}
             )
         })
-    })
-    .catch(error => res.status(500).json({error}))
+    } catch (error) {
+        res.status(500).json({error});
+    }
 }
