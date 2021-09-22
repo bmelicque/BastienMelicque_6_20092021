05cea20873c9e0d6d81844f0a28469b55fdb7840
Bastien Mélicque <bmelicque@gmail.com>
2021-09-22 14:52:14 +0200

    Reinforcing user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
+const {isEmail} = require('validator');
 
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    email: {
+        type: String,
+        required: true,
+        validate: [isEmail],
+        lowercase: true,
+        trimp: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        minLength: 6,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
